5ea6255e0b9499e5ca04eec0e81e9ab7724ee8b9
klaus19 <tejaskhartude@gmail.com>
2022-09-21 21:12:01 +0530

    Initial commit

---
+++ app/src/test/java/com/example/unittest1/EngineTest.kt
@@ -0,0 +1,19 @@
+package com.example.unittest1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class EngineTest {
+
+    private val engine = Engine(2000,189,50,false)
+
+    @Test
+    fun engineTurnsOn(){
+
+        engine.turnOn()
+        assertEquals(true,engine.isTurnedOn)
+        assertEquals(90,engine.temperature)
+
+    }
+}
